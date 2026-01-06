02f6f5fd8d688ab71586021a982fc45fb646c063
Matěj Přerovský <bofin@skaut.cz>
2026-01-06 23:44:36 +0100

    added working scrollwheel support with live updating and refactored some redundant code

--- sb-bri.sh
+++ sb-bri.sh
@@ -2,17 +2,20 @@
 
 case "$BLOCK_BUTTON" in
   1) /usr/bin/arandr & ;;
+  4) /usr/bin/brightnessctl -q set 5%+ ;;
+  5) /usr/bin/brightnessctl -q set 5%- ;;
 esac
 
-level="$(/usr/bin/brightnessctl get)"
+readonly level=$(/usr/bin/brightnessctl get)
 
+readonly color="^c#ffcc69^"
 if [ "$level" -le 33 ]; then
-  symbol="^c#ffcc69^󰃞"
+  symbol="󰃞"
 elif [ "$level" -le 66 ]; then
-  symbol="^c#ffcc69^󰃟"
+  symbol="󰃟"
 else
-  symbol="^c#ffcc69^󰃠"
+  symbol="󰃠"
 fi
 
 
-echo "$symbol $level^d^"
+echo "$color$symbol $level^d^"
